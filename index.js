44926a1cfddc9fb958720e70c235cb5477a31b5c
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-01-11 22:08:06 +0300

    entry point / all exports in one file

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports.Block = require('./lib/block');
+exports.Elem = require('./lib/elem');
+exports.ElemMod = require('./lib/elemmod');
+exports.Mod = require('./lib/mod');
+exports.Val = require('./lib/val');
