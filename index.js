524640026c0bada817c477dbe8bd0732e2448bf4
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-03-03 01:49:27 +0300

    Функция create, реализующая фабрику компонентов с элементами каррирования

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 'use strict';
 
-exports.Block = require('./lib/block');
-exports.Elem = require('./lib/elem');
-exports.ElemMod = require('./lib/elemmod');
-exports.Mod = require('./lib/mod');
-exports.Val = require('./lib/val');
+/**
+ * Возвращает функцию, которая позволяет вызывать конструтор без оператора new.
+ * @todo  Подумать о добавление св-ва constructor
+ * @param  {Function} constructor
+ * @return {Function}
+ */
+function wrap(constructor) {
+  return function () {
+    var component = Object.create(constructor.prototype);
+    constructor.apply(component, arguments);
+    return component;
+  }
+}
+
+/**
+ * @param  {String} component
+ * @return {Function|Object}
+ */
+exports.create = function (component) {
+  var constructor = wrap(function () {});
+  var slice = Array.prototype.slice;
+
+  if (arguments.length > 1) {
+    return constructor.apply(null, slice.call(arguments, 1));
+  } else {
+    return constructor;
+  }
+};
